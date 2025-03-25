d0576e6fbdce1d5070ad8321fd29b21c6cfdbfb8
vsurendranath1 <159035664+vsurendranath1@users.noreply.github.com>
2025-03-26 00:53:15 +0530

    Update stacks-map.js

--- stacks-map.js
+++ stacks-map.js
@@ -1,19 +1,7 @@
 const stacksMap = require('serverless-plugin-split-stacks').stacksMap;
 
-module.exports = (resources, logicalId) => {
-  const results = {};
+module.exports = (resource, logicalId) => {
+  if (logicalId.startsWith("ApiGateway")) return { destination: 'ApiGateway' };
 
-  for (const logicalId in resources) {
-    if (resources.hasOwnProperty(logicalId)) {
-      if (logicalId.startsWith("ApiGateway")) {
-        results[logicalId] = { destination: 'ApiGateway' };
-      } else {
-        // Handle other cases or fall back to a default if needed.
-        // For example, if you wanted to track all non-ApiGateway resources:
-        // results[logicalId] = { destination: 'Other' };
-      }
-    }
-  }
-
-  return results;
+  // Falls back to default
 }
