8a362585e01f010e27d568bfe3facf4593e34a44
vsurendranath1 <159035664+vsurendranath1@users.noreply.github.com>
2025-03-26 00:34:09 +0530

    Update stacks-map.js

--- stacks-map.js
+++ stacks-map.js
@@ -1,14 +1,7 @@
 const stacksMap = require('serverless-plugin-split-stacks').stacksMap;
 
-const movedApiGatewayIds = new Set(); // Track moved resources
-
 module.exports = (resource, logicalId) => {
-  if (logicalId.startsWith("ApiGateway")) {
-    if (movedApiGatewayIds.size < 10) {
-      movedApiGatewayIds.add(logicalId);
-      return { destination: "ApiGatewayStack" }; // Move first 10 to this stack
-    }
-  }
+  if (logicalId.startsWith("ApiGateway")) return { destination: 'ApiGateway' };
 
-  return {}; // Default behavior (keeps other ApiGateway resources in root stack)
-}
+  // Falls back to default
+};
