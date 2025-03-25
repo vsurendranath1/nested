3b86756cc80ccc7d2b6de16e9f5eee1489325b15
vsurendranath1 <159035664+vsurendranath1@users.noreply.github.com>
2025-03-25 23:55:59 +0530

    Update stacks-map.js

--- stacks-map.js
+++ stacks-map.js
@@ -1,5 +1,7 @@
 const stacksMap = require('serverless-plugin-split-stacks').stacksMap;
 
-module.exports = {
-  'AWS::ApiGateway::Method': { destination: 'ApiGatewayMethod' }
-}
+module.exports = (resource, logicalId) => {
+  if (logicalId.startsWith("ApiGateway")) return { destination: 'ApiGateway' };
+
+  // Falls back to default
+};
