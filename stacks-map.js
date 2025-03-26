f21ba032a38bcb6336ce3a1b2acd81c6faa2cf23
vsurendranath1 <159035664+vsurendranath1@users.noreply.github.com>
2025-03-26 22:59:46 +0530

    Update stacks-map.js

--- stacks-map.js
+++ stacks-map.js
@@ -1,20 +1,18 @@
 const stacksMap = require('serverless-plugin-split-stacks').stacksMap;
 
+let counter = 0;
+
 module.exports = (resource, logicalId) => {
-  if (logicalId.includes("ApiGateway")) {
-    return { destination: "ApiGatewayStack" };
-  } else if (logicalId.includes("LambdaFunction")) {
-    return { destination: "LambdaStack" };
-  } else if (logicalId.includes("LambdaPermission")) {
-    return { destination: "LambdaPermissionStack" };
-  } else if (logicalId.includes("IamRole") || logicalId.includes("Role")) {
-    return { destination: "IamRoleStack" };
-  } else if (logicalId.includes("LogGroup")) {
-    return { destination: "LogGroupStack" };
-  } else if (logicalId.includes("ServerlessDeployment")) {
-    return { destination: "ServerlessDeploymentStack" };
+  let stack;
+
+  if (logicalId) {
+    stack = counter % 2 === 0 ? "stackA" : "stackB"; // Distribute alternately
+    counter++; // Increment for next assignment
   } else {
-    return { destination: "OtherResourcesStack" };
+    stack = "OtherStack"; // If logicalId is missing, assign to OtherStack
   }
-}
+
+  return { destination: stack };
+};
+
 
