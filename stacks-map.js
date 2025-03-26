84f65cd9697d8372ca6154c38014138540939f44
vsurendranath1 <159035664+vsurendranath1@users.noreply.github.com>
2025-03-26 09:57:31 +0530

    Update stacks-map.js

--- stacks-map.js
+++ stacks-map.js
@@ -2,7 +2,7 @@ const stacksMap = require('serverless-plugin-split-stacks').stacksMap;
 
 
 module.exports = (resource, logicalId) => {
-  if(logicalId.includes("APiGateway")) return { destination:'APiGateway'};
+  if(logicalId.includes("ApiGatewayLogGroup")) return { destination:'APiGateway'};
   else if(logicalId.includes("Lambda")) return { destination:'Lambda'};
   return { destination: "other" };
   
