2cfdf06224607cdb5c505f4b0e9aa79c9cd51577
vsurendranath1 <159035664+vsurendranath1@users.noreply.github.com>
2025-03-30 15:46:21 +0530

    Delete stacks-map.js

--- stacks-map.js
+++
@@ -1,9 +0,0 @@
-const stacksMap = require('serverless-plugin-split-stacks').stacksMap;
-module.exports = (resource, Type) => {
-  if (Type === 'AWS::ApiGateway::Resource') {
-    return { destination: 'Dynamodb' };
-  }
-  return null; // Default case if the resource type is not matched
-};
-
-      
