65666ca7f12f310885496324db3f4adc350a4769
vsurendranath1 <159035664+vsurendranath1@users.noreply.github.com>
2025-03-30 14:46:48 +0530

    Update stacks-map.js

--- stacks-map.js
+++ stacks-map.js
@@ -1,4 +1,3 @@
-'use strict';
 const stacksMap = require('serverless-plugin-split-stacks').stacksMap;
 const memoize = require('memoizee');
 const namingUtils = require('serverless/lib/plugins/aws/lib/naming');
