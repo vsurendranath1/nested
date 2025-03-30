7b051e50f1b91ed7a933a6f6df50787e1537e76a
vsurendranath1 <159035664+vsurendranath1@users.noreply.github.com>
2025-03-30 18:56:38 +0530

    Update stacks-map.js

--- stacks-map.js
+++ stacks-map.js
@@ -1,1 +1,17 @@
+module.exports = {
+  processResources(resources) {
+    const stackNames = [
+      'Stack1', 'Stack2', 'Stack3', 'Stack4', 'Stack5',
+      'Stack6', 'Stack7', 'Stack8', 'Stack9', 'Stack10'
+    ];
+    const stackMapping = {};
+    let index = 0;
 
+    Object.keys(resources).forEach((resourceName) => {
+      stackMapping[resourceName] = stackNames[index % stackNames.length];
+      index++;
+    });
+
+    return stackMapping;
+  }
+};
