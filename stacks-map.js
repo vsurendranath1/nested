4156026fd7d38059c6846351089b8a00333586ed
vsurendranath1 <159035664+vsurendranath1@users.noreply.github.com>
2025-03-30 14:54:20 +0530

    Update stacks-map.js

--- stacks-map.js
+++ stacks-map.js
@@ -1,58 +1,20 @@
 const stacksMap = require('serverless-plugin-split-stacks').stacksMap;
-const memoize = require('memoizee');
-const namingUtils = require('serverless/lib/plugins/aws/lib/naming');
-
-const getApiGatewayResourceMap = memoize(serverless => {
-  const apiGatewayPlugin = serverless.pluginManager.plugins.find(
-    plugin => plugin.constructor.name === 'AwsCompileApigEvents'
-  );
-
-  const resourceMap = new Map();
-  const gatewayResourceLambdaMap = new Map();
-
-  apiGatewayPlugin.validated.events.map(({ functionName, http }) => {
-    const normalizedLambdaName = namingUtils.getNormalizedFunctionName(functionName);
-
-    resourceMap.set(
-      namingUtils.getMethodLogicalId(apiGatewayPlugin.getResourceName(http.path), http.method),
-      normalizedLambdaName
-    );
-
-    const tokens = [];
-    http.path.split('/').forEach(token => {
-      tokens.push(token);
-      const resourceName = namingUtils.getResourceLogicalId(tokens.join('/'));
-      if (!gatewayResourceLambdaMap.has(resourceName)) {
-        gatewayResourceLambdaMap.set(resourceName, new Set());
-      }
-      gatewayResourceLambdaMap.get(resourceName).add(normalizedLambdaName);
-    });
-  });
-
-  gatewayResourceLambdaMap.forEach((normalizedFunctionNames, resourceName) => {
-    if (normalizedFunctionNames.size > 1) return;
-    resourceMap.set(resourceName, normalizedFunctionNames.values().next().value);
-  });
-
-  return resourceMap;
-});
 
 require('serverless-plugin-split-stacks').resolveMigration = (resource, logicalId, serverless) => {
-  const typeBasedStack = {
-    'AWS::Lambda::Function': 'LambdaStack',
-    'AWS::ApiGateway::Method': 'ApiGatewayMethodStack',
-    'AWS::ApiGateway::Resource': 'ApiGatewayResourceStack',
-    'AWS::ApiGateway::RestApi': 'ApiGatewayRestApiStack',
-    'AWS::IAM::Role': 'IAMStack',
-    'AWS::Logs::LogGroup': 'LogGroupStack',
-    'AWS::DynamoDB::Table': 'DynamoDBStack',
-    'AWS::S3::Bucket': 'S3Stack'
-  };
-  const destination = typeBasedStack[resource.Type] || null;
-
-  console.log(`Resource: ${logicalId}, Type: ${resource.Type}, Assigned Stack: ${destination}`);
-
-
-  return typeBasedStack[resource.Type] ? { destination: typeBasedStack[resource.Type] } : null;
+  let destination;
+  switch (resource.Type) {
+    case 'AWS::ApiGateway::Method':
+    case 'AWS::ApiGateway::Resource':
+      destination = getApiGatewayResourceMap(serverless).get(logicalId);
+      break;
+    default:
+      // All other resource types if their name starts with one of the lambda names
+      // are propagated to given lambda stackxv
+      // (it's the Serverless internal convention to prefix most lambda specific resources
+      // with normalized lambda name)
+      destination = getAllNormalizedLambdaNames(serverless).find(normalizedLambdaName =>
+        logicalId.startsWith(normalizedLambdaName)
+      );
+  }
+  return destination ? { destination } : null;
 };
-
