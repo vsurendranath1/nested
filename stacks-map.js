6339ae01d40c2fb4a9e500e8aaa1c0d0aa2003a0
vsurendranath1 <159035664+vsurendranath1@users.noreply.github.com>
2025-03-30 19:12:29 +0530

    Update stacks-map.js

--- stacks-map.js
+++ stacks-map.js
@@ -1,18 +1,4 @@
-const stacksMap = require('serverless-plugin-split-stacks').stacksMap;
-module.exports = {
-  processResources(resources) {
-    const stackNames = [
-      'Stack1', 'Stack2', 'Stack3', 'Stack4', 'Stack5',
-      'Stack6', 'Stack7', 'Stack8', 'Stack9', 'Stack10'
-    ];
-    const stackMapping = {};
-    let index = 0;
-
-    Object.keys(resources).forEach((resourceName) => {
-      stackMapping[resourceName] = stackNames[index % stackNames.length];
-      index++;
-    });
-
-    return stackMapping;
-  }
+module.exports = (resources, logicalId) => {
+  if (logicalId.startsWith("ApiGateway")) return { destination: 'ApiGateway' };
+  // Falls back to default
 };
