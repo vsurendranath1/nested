7c22fe0af9dfe5d12f4daef0032f6565bdcdd28f
vsurendranath1 <159035664+vsurendranath1@users.noreply.github.com>
2025-03-30 16:35:26 +0530

    Update splitter.js

--- splitter.js
+++ splitter.js
@@ -1,3 +1,4 @@
+const stacksMap = require('serverless-plugin-split-stacks').stacksMap;
 module.exports = (resource, logicalId) => {
   if (logicalId.startsWith("ApiGateway")) return { destination: 'ApiGateway' };
   // Falls back to default
