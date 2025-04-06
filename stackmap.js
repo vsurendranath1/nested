1714dd3027b6bd9cf0c47fa9ee72c54b6724df09
vsurendranath1 <159035664+vsurendranath1@users.noreply.github.com>
2025-04-06 13:04:19 +0530

    Update stackmap.js

--- stackmap.js
+++ stackmap.js
@@ -1,1 +1,17 @@
+const stacksMap = require('serverless-plugin-split-stacks').stacksMap;
 
+module.exports = (resource, logicalId) => {
+  const apiTypes = [
+    'AWS::ApiGateway::Method',
+    'AWS::ApiGateway::Resource',
+    'AWS::ApiGateway::RestApi',
+    'AWS::ApiGateway::Deployment',
+    'AWS::ApiGateway::Stage',
+  ];
+
+  if (apiTypes.includes(resource.Type)) {
+    return 'api';
+  }
+
+  return null;
+};
